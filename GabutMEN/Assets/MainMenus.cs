2c59be081b67e7e7807e4685cb5eba06612303af
Raldienpr325 <aldinprayoga26@webmail.umm.ac.id>
2022-01-15 02:48:56 +0700

    lfs files

---
+++ GabutMEN/Assets/MainMenus.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenus : MonoBehaviour
+{
+    public void MulaiGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void KeluarGame()
+    {
+        Application.Quit();
+        Debug.Log("Keluar");
+    }
+}
